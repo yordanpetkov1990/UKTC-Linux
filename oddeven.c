bfc6154389b85825cd1b9f5ebe474fbfa047e4a9
yordanpetkov1990 <yordanpetkov1990@gmail.com>
2021-03-01 10:07:40 +0200

    My second commit

---
+++ oddeven.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int num;
+printf("Enter number");
+scanf("%d",&num);
+if(num % 2 ==0)
+{
+printf("Even\n");
+}
+else
+{
+printf("Odd\n");
+}
+return 0;
+}
